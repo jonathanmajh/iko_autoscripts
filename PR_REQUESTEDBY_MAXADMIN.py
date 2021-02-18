15c8f132cb382d4d7d0456b06008cccad1570a2d
Jonathan Ma <jonathanmajh@gmail.com>
2021-02-18 15:06:01 -0500

    change requested date to be future

--- PR_REQUESTEDBY_MAXADMIN.py
+++ PR_REQUESTEDBY_MAXADMIN.py
@@ -1,3 +1,10 @@
+# section for request date
+if status == 'ISSUED':
+    stat_date = mbo.getString('STATUSDATE')
+    new_date = stat_date.replace(stat_date.split(':')[1].split(' ')[0], str(int(stat_date.split(':')[1].split(' ')[0]) + 5))
+    request_date = new_date
+
+# section for requestedby
 if status == 'CAN' or status == 'CLOSE':
     pass
 elif 'MAXADMIN' not in requestedby:
@@ -7,6 +14,3 @@ else:
     planner = planners.getMbo(0)
     planner = planner.getString("RESPPARTY")
     requestedby = planner
-    warngroup = 'custom'
-    warnkey = 'debug'
-    warnparams = [planner]
