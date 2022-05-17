15620908f331ade433762135a7e60ea924c8873f
Jonathan Ma <jonathanmajh@gmail.com>
2022-05-17 10:41:01 -0400

    initial job plan revision

--- jobplan.save.py
+++ jobplan.save.py
@@ -3,5 +3,13 @@ from java.text import SimpleDateFormat
 
 if interactive == True:
     timeStamp = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime())
-    mbo.setValue('PLUSCCHANGEBY', user)
-    mbo.setValue('PLUSCCHANGEDATE', timeStamp)
+    if mboname == 'JOBPLAN':
+        mbo.setValue('PLUSCCHANGEBY', user)
+        mbo.setValue('PLUSCCHANGEDATE', timeStamp)
+    else:
+        jpnum = mbo.getString('jpnum')
+        jobplan = mbo.getMboSet("$JOBPLAN", "JOBPLAN", "JPNUM='" + jpnum + "'")
+        if jobplan.count() == 1:
+            jobplan = jobplan.getMbo(0)
+            jobplan.setValue('PLUSCCHANGEBY', user)
+            jobplan.setValue('PLUSCCHANGEDATE', timeStamp)
